58f0a29cd7b13ef8c0c68270a7ce034858471bb4
adi42wastaken <adityateraiyaself@gmail.com>
2026-08-08 00:08:22 +0530

    maybe I...maybe youuuu...(also give more codes wtf)

--- Homework_Assignment_bmath/Semester/Sem_1/Computer/umm.c
+++ Homework_Assignment_bmath/Semester/Sem_1/Computer/umm.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    int a = 49298734;
-    int b = 0;
-    int div = 1; 
-    while ((a / div) > 0)
-    {
-        b = b + (a % (div * 10)) / div;
-        div = div * 10;
+    int m1, m2, m3, m4, m5;
+
+    printf("Enter five marks: ");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+
+    int t = m1 + m2 + m3 + m4 + m5;
+    int a = t / 5;
+
+    if (m1 < 40 || m2 < 40 || m3 < 40 || m4 < 40 || m5 < 40) {
+        printf("Fail, D\n");
     }
-    printf("Is sum of digits of %d, %d?\n", a, b);
-    do
-    {
-        /* code */
-    } while (condition);
-    
+    else if (a >= 90) {
+        printf("PASS with Grade A\n");
+    }
+    else if (a >= 75 && a < 90) {
+        printf("PASS with Grade B\n");
+    }
+    else if (a >= 60 && a < 75) {
+        printf("PASS with Grade C\n");
+    }
+    else if (a >= 39 && a < 60) {
+        printf("PASS with Grade D\n");
+    }
+    else {
+        printf("invalid\n");
+    }
+
     return 0;
 }
